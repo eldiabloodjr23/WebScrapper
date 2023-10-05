c5a691593bfe8c2ed936adc6f3e1bca2dd450912
elchakal23 <moneyemoji.pro@gmail.com>
2023-10-06 00:11:22 +0200

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const navIcons = [
+  { src: '/assets/icons/search.svg', alt: 'search' },
+  { src: '/assets/icons/black-heart.svg', alt: 'heart' },
+  { src: '/assets/icons/user.svg', alt: 'user' },
+]
+
+const Navbar = () => {
+  return (
+    <header className="w-full">
+      <nav className="nav">
+        
+
+         
+
+       
+        
+        
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
